148cb4b6efa271589151ff3d30a75e80e9be5e31
Antonio <tonniloiola@gmail.com>
2020-08-10 23:08:08 -0300

    add spring feign

--- loja/src/main/java/br/com/tonni/microservico/loja/LojaApplication.java
+++ loja/src/main/java/br/com/tonni/microservico/loja/LojaApplication.java
@@ -3,10 +3,13 @@ package br.com.tonni.microservico.loja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LojaApplication {
 
 	@Bean
--- loja/src/main/java/br/com/tonni/microservico/loja/service/CompraService.java
+++ loja/src/main/java/br/com/tonni/microservico/loja/service/CompraService.java
@@ -1,29 +1,20 @@
 package br.com.tonni.microservico.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.tonni.microservico.loja.client.FornecedorClient;
 import br.com.tonni.microservico.loja.controller.dto.CompraDTO;
-import br.com.tonni.microservico.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO compra) {
 		
-		ResponseEntity<InfoFornecedorDTO> exchange = 
-				client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(), 
-				HttpMethod.GET,
-				null,
-				InfoFornecedorDTO.class);
-		
-		System.out.println(exchange.getBody().getEndereco());
+		System.out.println(fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado()).getEndereco());
 		
 	}
 
